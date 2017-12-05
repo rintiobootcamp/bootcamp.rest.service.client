80286e4f4eddec2e59e33a6879c975de1f374183
mosouley <mo.souley@gmail.com>
2017-12-05 17:57:03 +0100

    create commentaire client and its test

---
+++ src/main/java/com/bootcamp/client/CommentaireClient.java
@@ -0,0 +1,61 @@
+package com.bootcamp.client;
+
+
+import com.bootcamp.commons.utils.GsonUtils;
+import com.bootcamp.constants.AppConstant;
+import com.bootcamp.entities.Commentaire;
+import com.bootcamp.entities.Pilier;
+import com.bootcamp.utils.PropertiesFileUtils;
+import com.google.gson.reflect.TypeToken;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+public class CommentaireClient implements AppConstant {
+    RestTemplate restTemplate;
+    PropertiesFileUtils propertiesFileUtils;
+
+    public CommentaireClient() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        restTemplate = new RestTemplate(factory);
+        propertiesFileUtils= new PropertiesFileUtils();
+    }
+//
+//    public Commentaire create(Commentaire commentaire) throws IOException {
+//        String uri= propertiesFileUtils.getAppUrl("commentaire-service-fonctionnel-create-commentaire");
+//
+//        String requestBody = GsonUtils.toJSONWithoutClassName(commentaire);
+//        MultiValueMap<String, Object> headers = new LinkedMultiValueMap<String, Object>();
+//        headers.add("Accept", "application/json");
+//        headers.add("Content-Type", "application/json");
+//        HttpEntity request = new HttpEntity(requestBody, headers);
+//
+//        String apiResponse = restTemplate.postForObject(uri,
+//                request, String.class);
+//        commentaire = GsonUtils.getObjectFromJson(apiResponse, Commentaire.class);
+//
+//        return commentaire;
+//    }
+
+    public List<Commentaire> getByEntity(String entityType,int entityId) throws IOException{
+        propertiesFileUtils= new PropertiesFileUtils();
+        String uri=propertiesFileUtils.getAppUrl("commentaire-service-fonctionnel-get-Commentaire-by-entity");
+        String uriSufix="/"+entityType+"/"+entityId;
+        uri+=uriSufix;
+        ResponseEntity<String> response = restTemplate.getForEntity(uri,String.class);
+        String jsonData = response.getBody();
+        Type typeOfObjectsListNew = new TypeToken<List<Commentaire>>() {}.getType();
+        List<Commentaire> commentaires = GsonUtils.getObjectFromJson(jsonData,typeOfObjectsListNew);
+
+        return commentaires;
+
+    }
+}
---
+++ src/test/java/com/bootcamp/CommentaireClientTest.java
@@ -0,0 +1,22 @@
+package com.bootcamp;
+
+import com.bootcamp.client.CommentaireClient;
+import com.bootcamp.entities.Commentaire;
+import com.google.gson.Gson;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CommentaireClientTest {
+    @Test
+    public void getProjetByEntity() throws IOException {
+        String entityType = "PROJET";
+        int entityId = 1;
+        CommentaireClient commentaireClient = new CommentaireClient();
+        List <Commentaire> commentaires = commentaireClient.getByEntity(entityType,entityId);
+        Gson gson = new Gson();
+        String data = gson.toJson(commentaires);
+        System.out.println(data);
+    }
+}
